52623b13efe53cbc45701eaec7681906b8d7bc4b
wacosimple <110009518+wacomapua@users.noreply.github.com>
2022-10-29 23:57:42 +1000

    Laracast L8 EP35

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
         'posts' => Post::latest()->with('category', 'author')->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
 
@@ -40,7 +40,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
 
     ]);
 
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
